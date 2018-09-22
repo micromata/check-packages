bb286954f7d6d3539562d32dfd442bd338420158
Christian Kühl <c.kuehl@micromata.de>
2018-09-22 17:28:04 +0200

    adds tests for running cli

--- cli.js
+++ cli.js
@@ -2,13 +2,7 @@
 'use strict';
 
 const meow = require('meow');
-const updateNotifier = require('update-notifier');
-const pkg = require('./package.json');
-const chalk = require('chalk');
-const ora = require('ora');
-
-const readCliOptions = require('./lib/read-cli-options');
-const analyzeDependencies = require('./lib/analyze-dependencies');
+const run = require('./lib/run-cli');
 
 const cli = meow(`
   Usage
@@ -65,43 +59,4 @@ const cli = meow(`
   }
 });
 
-updateNotifier({ pkg }).notify();
-
-const options = readCliOptions(cli.input, cli.flags);
-
-if (options.valid && options.nextCliAction) {
-  cli[options.nextCliAction]();
-}
-
-if (!options.valid) {
-  console.log(chalk.red(`${options.error} Please check the help below:`));
-  cli.showHelp();
-}
-
-const spinner = ora('analysing dependencies...').start();
-const result = analyzeDependencies(options);
-
-if (result.error) {
-  spinner.fail(`Dependency analysis error: ${result.error}`);
-} else {
-  spinner.succeed('Dependencies analysed');
-}
-
-if (result.warning) {
-  console.log(chalk.black(chalk.bgYellow(result.warning)));
-}
-
-if (!result.error && result.unallowedPackages.length === 0) {
-  spinner.succeed('Congratulations, all dependencies are allowed!');
-  process.exit(0);
-}
-
-if (result.unallowedPackages.length) {
-  spinner.fail(`Found ${result.unallowedPackages.length} unallowed dependencies!`);
-
-  if (options.verbose) {
-    console.log(chalk.red(result.verboseOutput));
-  }
-}
-
-process.exit(1);
+run(cli);
