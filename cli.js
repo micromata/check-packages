61492b72b390cd8e254868a91e8bc2f42612c9bc
Christian Kühl <c.kuehl@micromata.de>
2018-09-24 21:01:42 +0200

    change description

--- cli.js
+++ cli.js
@@ -6,29 +6,36 @@ const run = require('./lib/run-cli');
 
 const cli = meow(`
   Usage
-    $ check-packages <list.json> [options]
+    $ check-packages <checklist.json> [options]
 
-  The content of the <list.json> file (or however you will name it) must be an array of package names (e.g. [ 'react', 'react-dom', 'redux', 'react-redux' ]).
+  Run 'check-packages' without checklist path and it will use the default
+  path 'packages-whitelist.json' (or 'packages-blacklist.json' when called
+  with flag --blacklist).
+  The content of the checklist file must be an array of package names
+  (e.g. [ 'react', 'react-dom', 'redux', 'react-redux' ]).
 
   Options
-    --topLevelOnly         Checks only direct dependencies listed in the top level package.json.
-                           Is equivalent to depth=0.
-                           default: false
-                           Note: You cannot use "depth" together with topLevelOnly
+    --topLevelOnly         Checks only direct dependencies listed in the
+                           top level package.json (equivalent to depth=0).
+                           Note: You cannot use --topLevelOnly together
+                           with --depth.
     --depth                Max depth of the dependency tree analysis.
                            default: Infinity
-                           Note: You cannot use "depth" together with topLevelOnly.
-    --blacklist    -black  Interpret list.json content as blacklisted dependency names.
-    --development  -dev    Analyze the dependency tree for packages in devDependencies.
-    --production   -prod   Analyze the dependency tree for packages in dependencies.
+                           Note: You cannot use --depth together
+                           with --topLevelOnly.
+    --blacklist    -black  Interpret content of checklist as blacklist.
+    --development  -dev    Analyze the dependency tree for devDependencies.
+    --production   -prod   Analyze the dependency tree for dependencies.
     --verbose              Lists unallowed dependencies.
     --version      -v      Displays the version number.
     --help         -h      Displays the help.
 
   Examples
-    $ package "check-packages whitelist.json --dev --depth=10"
-    $ package "check-packages whitelist.json --dev --topLevelOnly --verbose"
-    $ package "check-packages blacklist.json --prod --blacklist
+    $ package "check-packages"
+    $ package "check-packages --blacklist"
+    $ package "check-packages my-whitelist.json --dev --depth=10"
+    $ package "check-packages my-whitelist.json --dev --topLevelOnly --verbose"
+    $ package "check-packages my-blacklist.json --prod --blacklist
 `, {
   alias: {
     dev: 'development',
